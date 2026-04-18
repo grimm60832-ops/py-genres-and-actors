5ce3a3d84e6474ca27573788f7d8d624c43c9fe7
Grimm <grimm60832@gmail.com>
2026-04-18 12:34:51 +0300

    Solution

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from db.models import Genre, Actor
 
 def main() -> QuerySet:
     # Create
-    Genre.objects.create(name="Western",)
-    Genre.objects.create(name="Action",)
-    Genre.objects.create(name="Dramma",)
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    genres = ["Western", "Action", "Dramma"]
+    for g in genres:
+        Genre.objects.create(name=g)
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first, last in actors:
+        Actor.objects.create(first_name=first, last_name=last)
     # Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(last_name="Klooney").update(name="Clooney")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(
         first_name="Kianu",
         last_name="Reaves"
